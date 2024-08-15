a204e6df1630ed62b7141650d1d73fffeb40d64c
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-08-15 21:21:38 +0530

    Add files via upload

---
+++ _11patttern.java
@@ -0,0 +1,26 @@
+class _11patttern 
+{
+    public static void printPattern(int n)
+    {
+        int i, j;
+
+        // outer loop to handle rows
+        for (i = 0; i <= n; i++) {
+
+            // inner loop to handle columns
+            for (j = 0; j <= n; j++) {
+                System.out.print("*");
+            }
+
+            // printing new line for each row
+            System.out.println();
+        }
+    }
+
+    // Driver Function
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}
