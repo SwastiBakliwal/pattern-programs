57ef1760165845d50c6ecde22f7bcafda0a7ba7c
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-08-15 21:21:19 +0530

    Add files via upload

---
+++ _10pattern.java
@@ -0,0 +1,58 @@
+class _10pattern 
+{
+    public static void printPattern(int n)
+    {
+        int i, j;
+       // int num = 1;
+
+        // outer loop to handle upper part
+        for (i = 1; i <= n; i++) {
+            // inner loop to print stars
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // inner loop to print spaces
+            int spaces = 2 * (n - i);
+            for (j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+
+            // inner loop to print stars
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // outer loop to handle lower part
+        for (i = n; i >= 1; i--) {
+            // inner loop to print stars
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // inner loop to print spaces
+            int spaces = 2 * (n - i);
+            for (j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+
+            // inner loop to print stars
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    // Driver Function
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}
+    
