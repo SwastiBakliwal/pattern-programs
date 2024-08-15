24baedf5743e9b45a5d6d1112d255c182ef813a8
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-08-15 21:22:10 +0530

    Add files via upload

---
+++ _12pattern.java
@@ -0,0 +1,27 @@
+class _12pattern 
+{
+    public static void printPattern(int n)
+    {
+        int i, j;
+
+        // outer loop to handle rows
+        for (i = 1; i <= n; i++) {
+
+            // inner loop to handle columns
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // printing new line for each row
+            System.out.println();
+        }
+    }
+
+    // Driver Function
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}    
+
