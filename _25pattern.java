977fb3b9e07eae1f41d355b59ac9714faef24bb7
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-08-19 18:21:58 +0530

    Add files via upload

---
+++ _25pattern.java
@@ -0,0 +1,38 @@
+class _25pattern 
+{
+    public static void printPattern(int n)
+    {
+        int i, j;
+
+        // outer loop to handle rows
+        for (i = n; i >= 1; i--) {
+
+            // inner loop to handle columns
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // printing new line for each row
+            System.out.println();
+        }
+
+        // outer loop to handle rows
+        for (i = 2; i <= n; i++) {
+
+            // inner loop to handle columns
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // printing new line for each row
+            System.out.println();
+        }
+    }
+
+    // Driver Function
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }    
+}
