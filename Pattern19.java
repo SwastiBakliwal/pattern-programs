0c3b6682edfe01fcbe7c6f378026b2c0299347e7
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-11-18 23:08:54 +0530

    Add files via upload

---
+++ Pattern19.java
@@ -0,0 +1,20 @@
+class Pattern19 {
+    public static void main(String[] args) {
+        int i,j,n=5;
+        for(i=1;i<=n;i++){
+            int num =5;
+        for(j=1;j<=i;j++){
+            System.out.print(num);
+            num--;
+        }
+        System.out.println();
+    }
+    }    
+}
+/*
+5
+54
+543
+5432
+54321
+ */
