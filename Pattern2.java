66456320ec10f3fd8b942d1f825ce86a69d93d98
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-11-19 21:49:18 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,17 @@
+class Pattern2 {
+   public static void main(String[] args) {
+    int i ,j=1;
+    for(i=1;i<=5;i++){
+        for(j=1 ;j<=5 ;j++){
+      System.out.print(i);
+        }
+        System.out.println();
+    }
+   }    
+}
+/* 
+11111
+22222
+33333
+44444
+55555 */
