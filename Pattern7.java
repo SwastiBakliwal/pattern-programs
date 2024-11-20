ce10bc5237f76284a8f33bcc85f30b4b0d101aa4
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-11-20 23:05:48 +0530

    Add files via upload

---
+++ Pattern7.java
@@ -0,0 +1,18 @@
+class Pattern7 {
+   public static void main(String[] args) {
+    int i,j,num=1;
+    for(i=1;i<=4;i++){
+        for(j=1;j<=i;j++){
+            System.out.print(num); 
+            num++; 
+        }
+        System.out.println();
+    }
+   }    
+}
+/*
+1
+23
+456
+78910
+ */
