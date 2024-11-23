03a463e07c10e1a020459ed90c2bd27422366af2
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-11-23 19:51:05 +0530

    Add files via upload

---
+++ Pattern13.java
@@ -0,0 +1,21 @@
+class Pattern13 {
+  public static void main(String[] args) {
+    int i,j,n=5 ;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            if(i==n||j==1||i==j)
+            System.out.print("*");
+            else
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+  }    
+}
+/*
+*
+**
+* *
+*  *
+*****
+ */
